2dd51614086cb0b748467f11ab01eda49d9f4721
Gil Tayar <gil@tayar.org>
2022-02-04 11:54:52 +0200

    invalidate ESM imports that were valid in Node 10

--- scripts/convert-pure-esm-to-cjs.js
+++ scripts/convert-pure-esm-to-cjs.js
@@ -1,3 +1,6 @@
-const {execSync} = require('child_process');
+const { execSync } = require("child_process")
 
-execSync("esbuild --platform=node --bundle node_modules/import-meta-resolve/lib/resolve.js > lib/converted-esm/import-meta-resolve.js", {shell: true})
+execSync(
+    "esbuild --platform=node --bundle node_modules/import-meta-resolve/lib/resolve.js > lib/converted-esm/import-meta-resolve.js",
+    { shell: true }
+)
