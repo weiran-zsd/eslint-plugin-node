7002e3538d0ed22d3b16abc13e134b88f66cdba2
Sebastian Good <2230835+scagood@users.noreply.github.com>
2023-10-06 13:53:18 +0200

    feat: Detect sync functions from built in node modules

--- tests/lib/rules/no-sync.js
+++ tests/lib/rules/no-sync.js
@@ -5,91 +5,262 @@
 "use strict"
 
 const RuleTester = require("eslint").RuleTester
+const dedent = require("dedent")
 const rule = require("../../../lib/rules/no-sync")
 
-new RuleTester().run("no-sync", rule, {
+new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: "module",
+    },
+}).run("no-sync", rule, {
     valid: [
-        "var foo = fs.foo.foo();",
+        "var foo = readFile();",
+        dedent`
+            import { readFile as readFileSync } from 'node:fs';
+            readFileSync();
+        `,
         {
-            code: "var foo = fs.fooSync;",
+            code: dedent`
+                import { readFileSync } from 'node:fs';
+                readFileSync();
+            `,
             options: [{ allowAtRootLevel: true }],
         },
         {
-            code: "if (true) {fs.fooSync();}",
+            code: dedent`
+                import { readFileSync } from 'fs';
+                readFileSync();
+            `,
+            options: [{ allowAtRootLevel: true }],
+        },
+        {
+            code: dedent`
+                import { readFileSync } from 'fs';
+                if (true) {
+                    readFileSync();
+                }
+            `,
             options: [{ allowAtRootLevel: true }],
         },
     ],
     invalid: [
         {
-            code: "var foo = fs.fooSync();",
+            code: dedent`
+                import { readFileSync } from 'fs';
+                readFileSync();
+            `,
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "var foo = fs.fooSync();",
-            options: [{ allowAtRootLevel: false }],
+            code: dedent`
+                import { readFileSync } from 'fs';
+                if (true) {
+                    readFileSync();
+                }
+            `,
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "if (true) {fs.fooSync();}",
+            code: dedent`
+                import { readFileSync } from 'fs';
+                function soSomethingSync() {
+                    readFileSync();
+                }
+            `,
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+
+        {
+            code: dedent`
+                import { readFileSync } from 'node:fs';
+                readFileSync();
+            `,
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "var foo = fs.fooSync;",
+            code: dedent`
+                import fs from 'node:fs';
+                fs.readFileSync();
+            `,
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "function someFunction() {fs.fooSync();}",
+            code: dedent`
+                import * as fs from 'node:fs';
+                fs.readFileSync();
+            `,
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "function someFunction() {fs.fooSync();}",
+            code: dedent`
+                const { readFileSync } = require('node:fs');
+                readFileSync();
+            `,
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+        {
+            code: dedent`
+                const fs = require('node:fs');
+                fs.readFileSync();
+            `,
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+
+        {
+            code: dedent`
+                import { readFileSync as readFile } from 'node:fs';
+                readFile();
+            `,
+            errors: [
+                {
+                    messageId: "noSyncAlias",
+                    data: {
+                        importName: "readFileSync",
+                        aliasName: "readFile",
+                    },
+                },
+            ],
+        },
+        {
+            code: dedent`
+                const { readFileSync: readFile } = require('node:fs');
+                readFile();
+            `,
+            errors: [
+                {
+                    messageId: "noSyncAlias",
+                    data: {
+                        importName: "readFileSync",
+                        aliasName: "readFile",
+                    },
+                },
+            ],
+        },
+
+        {
+            code: dedent`
+                import { readFileSync } from 'fs';
+                function something() {
+                    readFileSync();
+                }
+            `,
             options: [{ allowAtRootLevel: true }],
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
         {
-            code: "var a = function someFunction() {fs.fooSync();}",
+            code: dedent`
+                const { readFileSync } = require('fs');
+                function something() {
+                    readFileSync();
+                }
+            `,
             options: [{ allowAtRootLevel: true }],
             errors: [
                 {
-                    messageId: "noSync",
-                    data: { propertyName: "fooSync" },
-                    type: "MemberExpression",
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+        {
+            code: dedent`
+                import fs from 'node:fs';
+                function something() {
+                    fs.readFileSync();
+                }
+            `,
+            options: [{ allowAtRootLevel: true }],
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+        {
+            code: dedent`
+                const fs = require('fs');
+                function something() {
+                    fs.readFileSync();
+                }
+            `,
+            options: [{ allowAtRootLevel: true }],
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+
+        {
+            code: dedent`
+                import { readFileSync } from 'fs';
+                readFileSync.apply(null, '/something');
+            `,
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
+                },
+            ],
+        },
+
+        {
+            code: dedent`
+                import * as fs from 'fs';
+                fs.readFileSync.apply(null, '/something');
+            `,
+            errors: [
+                {
+                    messageId: "noSyncMember",
+                    data: { importName: "readFileSync" },
                 },
             ],
         },
