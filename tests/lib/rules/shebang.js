b383b4971df4f4b67099655797b654b36d6a8fdf
Louis Orleans <louis@orleans.io>
2024-03-05 01:24:36 -0800

    feat(shebang): add support for env's split-string option (#195)
    * feat(shebang): add support for env's split-string option
    
    This is useful for passing arguments to node
    
    * feat(shebang): support all `env` options
    
    * feat(shebang): improve regex performance

--- tests/lib/rules/shebang.js
+++ tests/lib/rules/shebang.js
@@ -42,6 +42,31 @@ ruleTester.run("shebang", rule, {
             code: "#!/usr/bin/env node\nhello();",
         },
         {
+            name: "string-bin/bin/test.js",
+            filename: fixture("string-bin/bin/test.js"),
+            code: "#!/usr/bin/env -S node\nhello();",
+        },
+        {
+            name: "string-bin/bin/test.js",
+            filename: fixture("string-bin/bin/test.js"),
+            code: "#!/usr/bin/env -S node --loader tsm\nhello();",
+        },
+        {
+            name: "string-bin/bin/test.js",
+            filename: fixture("string-bin/bin/test.js"),
+            code: "#!/usr/bin/env --ignore-environment node\nhello();",
+        },
+        {
+            name: "string-bin/bin/test.js",
+            filename: fixture("string-bin/bin/test.js"),
+            code: "#!/usr/bin/env -i -S node --loader tsm\nhello();",
+        },
+        {
+            name: "string-bin/bin/test.js",
+            filename: fixture("string-bin/bin/test.js"),
+            code: "#!/usr/bin/env --block-signal=SIGINT -S FOO=bar node --loader tsm\nhello();",
+        },
+        {
             name: "object-bin/bin/c.js",
             filename: fixture("object-bin/bin/c.js"),
             code: "hello();",
