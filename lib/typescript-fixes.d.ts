5ea136a96f396f40266824db58facafa96df0981
scagood <2230835+scagood@users.noreply.github.com>
2024-04-03 22:12:31 +0100

    chore: Add comment for the "ts-expect-error"

--- lib/typescript-fixes.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface ArrayConstructor {
-        // https://github.com/microsoft/TypeScript/issues/17002
-        isArray(arg: ReadonlyArray<unknown> | unknown): arg is ReadonlyArray<unknown>
-    }
-}
-
-export {}
