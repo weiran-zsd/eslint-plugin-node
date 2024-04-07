09fdc52b0b485243dff7c75d236773c21777476e
Pelle Wessman <pelle@kodfabrik.se>
2024-04-07 17:35:04 +0200

    Fix

--- lib/eslint-utils.d.ts
+++ lib/eslint-utils.d.ts
@@ -1,7 +1,6 @@
 declare module "eslint-plugin-es-x" {
-    export const rules: NonNullable<
-// @ts-ignore
-import('eslint').ESLint.Plugin["rules"]>;
+    // @ts-ignore
+    export const rules: NonNullable<import('eslint').ESLint.Plugin["rules"]>;
 }
 
 declare module "@eslint-community/eslint-utils" {
